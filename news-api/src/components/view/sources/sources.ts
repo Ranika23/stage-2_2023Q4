dc366987f68cc0b4b7634a339fb97b44dbfa213c
Ranika23 <bveronikal@gmail.com>
2024-03-05 12:43:02 +0300

    feat: add key selection function

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -5,26 +5,6 @@ function Errors(): void {
 }
 class Sources {
     draw(data: NewsSoursesResponce[]) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp: HTMLFormElement | null = document.querySelector('#sourceItemTemp');
-
-        data.forEach((item) => {
-            if (sourceItemTemp === null) Errors();
-            else {
-                const sourceClone = sourceItemTemp.content.cloneNode(true);
-                sourceClone.querySelector('.source__item-name').textContent = Object(item).name;
-                sourceClone.querySelector('.source__item').setAttribute('data-source-id', Object(item).id);
-
-                fragment.append(sourceClone);
-            }
-        });
-
-        const elemSources: HTMLFormElement | null = document.querySelector('.sources');
-        if (elemSources === null) Errors();
-        else {
-            elemSources.append(fragment);
-        }
-
         const sourceItemChar: HTMLFormElement | null = document.querySelector('#sourceItemChar');
         const elemChar: HTMLFormElement | null = document.querySelector('.buttons-char');
         if (elemChar === null) Errors();
@@ -33,10 +13,52 @@ class Sources {
                 if (sourceItemChar === null) Errors();
                 else {
                     const charsClone = sourceItemChar.content.cloneNode(true);
-                    charsClone.querySelector('.source__char-item').textContent = String.fromCharCode(i);
+                    charsClone.querySelector('.source__char').textContent = String.fromCharCode(i);
                     elemChar.append(charsClone);
                 }
             }
+        }
+
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLFormElement | null = document.querySelector('#sourceItemTemp');
+        const elemSources: HTMLFormElement | null = document.querySelector('.sources');
+        const elemChars = document.querySelector('.buttons-char');
+        if (elemChars === null) Errors();
+        else {
+            elemChars.addEventListener('click', (e) => {
+                if (elemSources === null) Errors();
+                else elemSources.innerHTML = '';
+                data.forEach((item) => {
+                    if (elemSources === null) Errors();
+
+                    if (sourceItemTemp === null) Errors();
+                    else {
+                        if (elemChars === null) Errors();
+                        else {
+                            if (elemSources === null) Errors();
+
+                            if (elemSources !== null) {
+                                if (!(e.target instanceof HTMLElement)) return;
+
+                                const itemChar: HTMLElement = e.target;
+                                if (itemChar.textContent === Object(item).name[0]) {
+                                    const sourceClone = sourceItemTemp.content.cloneNode(true);
+                                    sourceClone.querySelector('.source__item-name').textContent = Object(item).name;
+                                    sourceClone
+                                        .querySelector('.source__item')
+                                        .setAttribute('data-source-id', Object(item).id);
+                                    fragment.append(sourceClone);
+
+                                    if (elemSources === null) Errors();
+                                    else {
+                                        elemSources.append(fragment);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            });
         }
     }
 }
